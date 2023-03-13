d774aa0b1f26a45b71b57bea12015d7982c90a0c
saifelleuch <sa2014if@gmail.com>
2023-03-13 23:01:51 +0100

    another one

--- 0x00-hello-dart/4-print_string.dart
+++ 0x00-hello-dart/4-print_string.dart
@@ -1,5 +1,5 @@
 void main() {
   String str = "Holberton School";
-  print('${str}${str}${str}\n');
-  print('${str.substring(1, 10)}\n');
+  print('${str}${str}${str}');
+  print('${str.substring(0, 9)}');
   print('\n');}
