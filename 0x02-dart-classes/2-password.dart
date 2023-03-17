c734c4b554a70a0acd25f094270a5d869bf3d85e
saifelleuch <sa2014if@gmail.com>
2023-03-17 23:23:27 +0100

    another one

---
+++ 0x02-dart-classes/2-password.dart
@@ -0,0 +1,15 @@
+class Password {
+  String password = '';
+
+  
+  Password({String password = ''}) : password = password;
+
+  bool isValid() {
+    RegExp hasUppercase = RegExp(r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,16}$');
+    return hasUppercase.hasMatch(password);
+  }
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}
