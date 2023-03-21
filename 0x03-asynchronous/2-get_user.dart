eef07852642443af4b0199da1424b53f48f7cd06
saifelleuch <sa2014if@gmail.com>
2023-03-21 22:04:26 +0100

    another one

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,10 @@
+import '2-util.dart';
+
+Future<void> getUser() async {
+  try {
+    var userData = await fetchUser();
+    print(userData);
+  } catch (error) {
+    print('error caught: $error');
+  }
+}
